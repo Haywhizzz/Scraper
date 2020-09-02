1e36adfb91b8c82454efb139004ff8d0739f5dff
haywhizzz <haywhizzz@gmail.com>
2020-09-02 18:53:51 +0100

    Add initialize method

--- lib/firstbank.rb
+++ lib/firstbank.rb
@@ -2,5 +2,11 @@ require 'nokogiri'
 require 'httparty'
 
 class Firstbank
+  attr_reader :firstbank_locations, :url
+
+  def initialize
+    @url = 'https://www.firstbanknigeria.com/list-of-open-branches-nationwide/'
+    @firstbank_locations = []
+  end
     
 end
