93927171d5e275745b81f9fb55a71cc062897f3a
haywhizzz <haywhizzz@gmail.com>
2020-09-02 18:54:32 +0100

    Add scrape method

--- lib/firstbank.rb
+++ lib/firstbank.rb
@@ -8,5 +8,17 @@ class Firstbank
     @url = 'https://www.firstbanknigeria.com/list-of-open-branches-nationwide/'
     @firstbank_locations = []
   end
+
+  def scrape(csv)
+    parsed_page = parse_url(@url)
+    parsed_page.css('div.wpb_wrapper').css('tr > td').map do |address|
+      firstbank = []
+      location_address = address.text
+      @firstbank_locations.push(location_address)
+      firstbank.push(location_address)
+      csv << firstbank
+    
+    end
+  end
     
 end
