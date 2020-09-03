4d63dc15c6d969951375da2a46fd673acadacc37
haywhizzz <haywhizzz@gmail.com>
2020-09-03 18:26:48 +0100

    Checking for rspec error

--- bin/main.rb
+++ bin/main.rb
@@ -1,9 +1,9 @@
 # !/usr/bin/env ruby
 
 require 'csv'
-require_relative '../lib/firstbank.rb'
+require_relative '../lib/first_bank.rb'
 
-new_csv = CSV.open('firstbank.csv', 'a+')
+new_csv = CSV.open('first_bank.csv', 'a+')
 new_csv << %w[area]
 
 firstbank = FirstBank.new
