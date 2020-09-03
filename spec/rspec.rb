6f17a448349e67ab50ea16909ae9ef40b3761f2f
haywhizzz <haywhizzz@gmail.com>
2020-09-03 09:09:51 +0100

    Add tests file

--- spec/rspec.rb
+++ spec/rspec.rb
@@ -1,25 +1,25 @@
-require './lib/tastee'
+require './lib/firstbank'
 
 describe Firstbank do
-  let(:tastee) { Firstbank.new }
+  let(:firstbank) { Firstbank.new }
   describe '#initialize' do
     it 'initialize url to string value' do
-      expect(tastee.url).to be_a(String)
+      expect(firstbank.url).to be_a(String)
     end
     it 'initialize firstbank_location to an empty array' do
-      expect(tastee.tastee_locations).to eql([])
+      expect(firstbank.firstbank_locations).to eql([])
     end
     it 'initialize parsed_page to null' do
-      expect(tastee.parsed_page.nil?).to eql(true)
+      expect(firstbank.parsed_page.nil?).to eql(true)
     end
   end
 
   describe '#scrape' do
-    let(:csv) { CSV.open('csv_output/tastee1.csv', 'a+') }
-    let(:update_csv) { csv << %w[area address delivery] }
+    let(:csv) { CSV.open('csv_output/firstbank1.csv', 'a+') }
+    let(:update_csv) { csv << %w[area] }
 
     it 'should update the location array' do
-      tastee.scrape(update_csv)
+      firstbank.scrape(update_csv)
       expect(firstbank.firstbank_locations.length).to be > 0
     end
     it 'should assign a value to parse_page' do
